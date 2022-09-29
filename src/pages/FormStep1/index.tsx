b56ecb1316aaf111189932d875319f69931ad96e
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-29 11:29:14 -0300

    :tada: init

---
+++ src/pages/FormStep1/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export const FormStep1 = () => {
+    return(
+        <Container>
+            Step 1
+        </Container>
+    )
+}
---
+++ src/pages/FormStep2/index.tsx
@@ -0,0 +1,9 @@
+import { Container } from "./styles";
+
+export const FormStep2 = () => {
+    return(
+        <Container>
+            Step 2
+        </Container>
+    )
+}
