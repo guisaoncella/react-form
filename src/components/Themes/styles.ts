1ee18a3eb22c5c6576035ec5c963be22e77efada
Guilherme Saoncella da Silva <guisaoncella@gmail.com>
2022-09-30 16:01:42 -0300

    :sparkles: feat: form step 1

--- src/components/Themes/styles.ts
+++ src/components/Themes/styles.ts
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    background-color: #02044A;
+    background-color: #000814;
     color: #fff;
     min-height: 100vh;
 `
--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,11 +1,55 @@
+import { useNavigate } from "react-router-dom";
 import { Theme } from "../../components/Themes";
 import { Container } from "./styles";
+import { useForm, FormActions } from "../../contexts/FormContext";
+import { ChangeEvent, useEffect } from "react";
 
 export const FormStep1 = () => {
+    const navigate = useNavigate();
+    const {state, dispatch} = useForm()
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        })
+    },[])
+
+    const handleNextStep = () => {
+        if(state.name == ''){
+            alert('Preencha todos os campos!')
+            return
+        }
+        navigate('/step2')
+    }
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
+
     return(
         <Theme>
             <Container>
-                Step 1
+                <p>Passo 1/3 - {state.name}</p>
+                <h1>Vamos começar com o seu nome</h1>
+                <p>Preencha o campo abaixo com seu nome completo.</p>
+
+                <hr/>
+
+                <label>
+                    Seu nome completo
+                    <input
+                        type="text"
+                        autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
+                    />
+
+                    <button onClick={handleNextStep}>Próximo</button>
+                </label>
             </Container>
         </Theme>
     )
